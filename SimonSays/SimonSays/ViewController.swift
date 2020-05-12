09af03c0f56142726cce24452a153adb566ba275
Thomas Cacciatore <tcacciatore597@gmail.com>
2020-05-12 18:55:10 -0400

    Setup Storyboard and basic elements needed for Simon Says game.

---
+++ SimonSays/SimonSays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Thomas Cacciatore on 5/12/20.
+//  Copyright © 2020 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
