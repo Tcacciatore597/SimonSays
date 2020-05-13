bde994c3feefc547e5284424f61b351be8b4eac8
Thomas Cacciatore <tcacciatore597@gmail.com>
2020-05-13 18:25:28 -0400

    Connected outlets and actions for buttons and labels.  Implemented tags for buttons and labels.  Set logic to visually show a user when a button is tapped by changing the alpha.

--- SimonSays/SimonSays/ViewController.swift
+++ SimonSays/SimonSays/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorButtons: [CircularButton]!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet var playerLabels: [UILabel]!
+    @IBOutlet var scoreLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorButtons = colorButtons.sorted() {
+            $0.tag < $1.tag
+        }
+        playerLabels = playerLabels.sorted() {
+            $0.tag < $1.tag
+        }
+        scoreLabels = scoreLabels.sorted() {
+            $0.tag < $1.tag
+        }
+        createNewGame()
     }
-
+    
+    func createNewGame() {
+        actionButton.setTitle("Start Game", for: .normal)
+        for button in colorButtons {
+            button.alpha = 0.5
+        }
+    }
+    
+    @IBAction func colorButtonHandler(_ sender: CircularButton) {
+        print("Button \(sender.tag) tapped")
+    }
+    
+    @IBAction func actionButtonHandler(_ sender: UIButton) {
+        print("Action tapped")
+    }
+    
 
 }
 
